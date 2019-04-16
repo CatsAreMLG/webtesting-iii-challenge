efb761ced43294c5244a286bf8dca117ab6e7ae6
CatsAreMLG <joshuadgon@gmail.com>
2019-04-16 16:28:28 -0500

    Finish display component tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -13,14 +13,41 @@ describe('<Display />', () => {
     render(<Display closed="true" locked="true" />)
     cleanup()
   })
+  it('should default to unlocked', () => {
+    const { getByText } = render(<Display />)
+    getByText(/unlocked/i)
+    cleanup()
+  })
+  it('should default to open', () => {
+    const { getByText } = render(<Display />)
+    getByText(/open/i)
+    cleanup()
+  })
   it('should be locked', () => {
-    const { getByText } = render(<Display closed="true" locked="true" />)
+    const { getByText } = render(<Display closed={true} locked={true} />)
     getByText(/locked/i)
     cleanup()
   })
   it('should be closed', () => {
-    const { getByText } = render(<Display closed="true" locked="true" />)
+    const { getByText } = render(<Display closed={true} locked={true} />)
     getByText(/closed/i)
+    cleanup()
+  })
+  it('should be unlocked', () => {
+    const { getByText } = render(<Display closed={false} locked={false} />)
+    getByText(/unlocked/i)
+    cleanup()
+  })
+  it('should be green-led', () => {
+    const { getByText } = render(<Display closed={false} locked={false} />)
+    expect(getByText(/open/i).classList[1]).toEqual('green-led')
+    expect(getByText(/unlocked/i).classList[1]).toEqual('green-led')
+    cleanup()
+  })
+  it('should be red-led', () => {
+    const { getByText } = render(<Display closed={true} locked={true} />)
+    expect(getByText(/closed/i).classList[1]).toEqual('red-led')
+    expect(getByText(/locked/i).classList[1]).toEqual('red-led')
     cleanup()
   })
 })
