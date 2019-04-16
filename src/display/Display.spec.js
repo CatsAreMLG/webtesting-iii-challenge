ad7c6ebe9ae28a9e22d7adf4027d3a3a17a17b76
CatsAreMLG <joshuadgon@gmail.com>
2019-04-16 17:17:02 -0500

    Remove cleanup add cleanup after each

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { render, fireEvent, cleanup } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
+import 'react-testing-library/cleanup-after-each'
 
 import Display from './Display'
 
@@ -11,43 +12,35 @@ describe('<Display />', () => {
   })
   it('should render', () => {
     render(<Display closed="true" locked="true" />)
-    cleanup()
   })
   it('should default to unlocked', () => {
     const { getByText } = render(<Display />)
     getByText(/unlocked/i)
-    cleanup()
   })
   it('should default to open', () => {
     const { getByText } = render(<Display />)
     getByText(/open/i)
-    cleanup()
   })
   it('should be locked', () => {
     const { getByText } = render(<Display closed={true} locked={true} />)
     getByText(/locked/i)
-    cleanup()
   })
   it('should be closed', () => {
     const { getByText } = render(<Display closed={true} locked={true} />)
     getByText(/closed/i)
-    cleanup()
   })
   it('should be unlocked', () => {
     const { getByText } = render(<Display closed={false} locked={false} />)
     getByText(/unlocked/i)
-    cleanup()
   })
   it('should be green-led', () => {
     const { getByText } = render(<Display closed={false} locked={false} />)
     expect(getByText(/open/i).classList[1]).toEqual('green-led')
     expect(getByText(/unlocked/i).classList[1]).toEqual('green-led')
-    cleanup()
   })
   it('should be red-led', () => {
     const { getByText } = render(<Display closed={true} locked={true} />)
     expect(getByText(/closed/i).classList[1]).toEqual('red-led')
     expect(getByText(/locked/i).classList[1]).toEqual('red-led')
-    cleanup()
   })
 })
