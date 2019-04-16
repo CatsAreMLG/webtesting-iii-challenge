40087aa16be17ce37538f4de5a9ae2d132a4b8b7
CatsAreMLG <joshuadgon@gmail.com>
2019-04-16 15:42:33 -0500

    Add locked and closed check

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { render, fireEvent, cleanup } from 'react-testing-library'
 
 import Display from './Display'
 
 describe('<Display />', () => {
   it('should match snapshot', () => {
     const tree = renderer.create(<Display />).toJSON()
-
     expect(tree).toMatchSnapshot()
+  })
+  it('should render', () => {
+    render(<Display closed="true" locked="true" />)
+    cleanup()
+  })
+  it('should be locked', () => {
+    const { getByText } = render(<Display closed="true" locked="true" />)
+    getByText(/locked/i)
+    cleanup()
+  })
+  it('should be closed', () => {
+    const { getByText } = render(<Display closed="true" locked="true" />)
+    getByText(/closed/i)
+    cleanup()
   })
 })
